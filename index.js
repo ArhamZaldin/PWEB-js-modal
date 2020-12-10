67493ce3b5adb22445610c707e074993cdfbf66c
Arham Zainul Abidin <71875242+ArhamZaldin@users.noreply.github.com>
2020-12-11 04:35:51 +0700

    maaf

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-let username = document.forms['myForm']['username'].value;
-let password = document.forms['myForm']['password'].value;
-let body = document.getElementById('body');
-let button = document.querySelector('button[data-toggle="login"]');
-let modal = document.querySelector('.modal');
+function login_act() {
+    let username = document.forms['login']['username'].value;
+    let password = document.forms['login']['password'].value;
+    let body = document.getElementById('body');
+    let modal = document.querySelector('.modal');
+    let invalid = document.querySelector('.span');
+    // let button = document.querySelector('button[data-toggle="login"]');
 
-function login() {
     if (username == "pweb_C" && password == "123123") {
-        function changeBodyColor() {
-            body.style.background = 'green';
-        }
-        
-        button.addEventListener('click', function() {
-            modal.classList.add('show');
-        })
-        
+        modal.classList.add('show');
+        invalid.classList.remove('show');
+        body.style.background = 'green';
         modal.addEventListener('click', function() {
             this.classList.remove('show');
             this.querySelector('.modal-content').addEventListener('click', function(e) {
                 e.stopPropagation();
             })
-        }) 
+        })
+    }
+
+    else {
+        invalid.classList.add('show');
+        body.style.background = 'white';
     }
 }
 
 
+// button.addEventListener('click', function() {
+//     modal.classList.add('show');
+// })
+
+// modal.addEventListener('click', function() {
+//     this.classList.remove('show');
+//     this.querySelector('.modal-content').addEventListener('click', function(e) {
+//         e.stopPropagation();
+//     })
+// }) 
+// function changeBodyColor() {
+//     body.style.background = 'green';
+// }
